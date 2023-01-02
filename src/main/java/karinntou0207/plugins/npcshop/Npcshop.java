cda9e075a1ae48bcdaf6433193b1589780caff93
Karintou0207 <118284119+Karintou0207@users.noreply.github.com>
2023-01-02 21:53:46 +0900

    Initial commit

---
+++ src/main/java/karinntou0207/plugins/npcshop/Npcshop.java
@@ -0,0 +1,17 @@
+package karinntou0207.plugins.npcshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Npcshop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
